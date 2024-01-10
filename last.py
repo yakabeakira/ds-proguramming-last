200da45e62947dcc71522fe630ece5c92dea3549
yakabeakira <s2322091@stu.musashino-u.ac.jp>
2024-01-10 23:11:34 +0900

    コードまっさらにする

--- last.py
+++ last.py
@@ -1,18 +1,2 @@
-import sqlite3
-from bs4 import BeautifulSoup
-import requeｓts
-path = '/Users/yakabe/hasegawa/last12/ds-proguramming-last/'
 
-#DBファイルを保存するためのファイルパス
-db_name = 'wheatherindex.sqlite'
-con = sqlite3.connect(path + db_name)
-con.close()
-
-#テーブルを作成
-con = sqlite3.connect(path + db_name)
-cur = con.cursor()
-#テーブルにカラムを作成
-sql_create_table_wheatherindex = 'CREATE TABLE wheather_index(cityday TEXT, wheatherindex TEXT, good_sleep_score FLOAT);'
-cur.execute(sql_create_table_wheatherindex)
-con.close()
 
