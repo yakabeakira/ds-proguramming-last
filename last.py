e1220eb65107cf0ad61137cb70ca2a50c9556710
yakabeakira <s2322091@stu.musashino-u.ac.jp>
2024-01-12 03:24:47 +0900

    ファイルパスとテーブルを作成

--- last.py
+++ last.py
@@ -4,3 +4,19 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+#DBファイルを保存するためのパスを作成
+path = '/Users/yakabe/hasegawa/last12/ds-proguramming-last/'
+#DBファイル名
+db_name = 'sleepindex.sqlite'
+con = sqlite3.connect(path + db_name)
+con.close()
+
+#テーブル作成
+con = sqlite3.connect(path + db_name)
+
+cur = con.cursor()
+
+sql_create_table_sleep = 'CREATE TABLE sleepindex(sleep_index TEXT);'
+cur.execute(sql_create_table_sleep)
+
+con.close()
